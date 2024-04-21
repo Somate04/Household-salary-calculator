b3314cd2fdb7ac16fcd7b698e32eec5f096fd491
Huszár Máté Soma <hfujf8@inf.elte.hu>
2024-04-21 11:03:58 +0200

    Added colors to entitled text

--- react-beadando-berkalkulator/src/components/SalaryCalculator/components/Entitled.jsx
+++ react-beadando-berkalkulator/src/components/SalaryCalculator/components/Entitled.jsx
@@ -1,10 +1,19 @@
 import { useMyContext } from "../../../Context";
+import styles from "./Entitled.module.css";
 
 function Entitled() {
   const { checkedNewlyWed, newlyWedDiscount } = useMyContext();
   const text = newlyWedDiscount ? "Jogosult" : "Nem jogosult";
+  const style = newlyWedDiscount ? styles.entitled : styles.notEntitled;
 
-  return <p style={{ display: checkedNewlyWed ? "block" : "none" }}>{text}</p>;
+  return (
+    <p
+      className={style}
+      style={{ display: checkedNewlyWed ? "block" : "none" }}
+    >
+      {text}
+    </p>
+  );
 }
 
 export default Entitled;
